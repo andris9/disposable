c634583ffaca3d6cb6c8f50e4c14535b1d76a054
Andris Reinman <andris@kreata.ee>
2013-03-08 10:21:13 +0200

    jshinted code

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 var api = require("./lib/api"),
-	web = require("./lib/web"),
-	smtp = require("./lib/smtp"),
-	pop3 = require("./lib/pop3"),
-	ready = 0;
+    web = require("./lib/web"),
+    smtp = require("./lib/smtp"),
+    pop3 = require("./lib/pop3"),
+    ready = 0;
 
 api.initDB(function(err){
-	if(err){
-		throw err;
-	}
+    if(err){
+        throw err;
+    }
 
-	web.listen(listener.bind(this, "Web"));
-	smtp.listen(listener.bind(this, "SMTP"));
-	pop3.listen(listener.bind(this, "POP3"));
+    web.listen(listener.bind(this, "Web"));
+    smtp.listen(listener.bind(this, "SMTP"));
+    pop3.listen(listener.bind(this, "POP3"));
 });
 
 function listener(service, error){
-	if(error){
-		console.log("Starting " + service + " server failed for the following error:");
-		console.log(error);
-		return process.exit(1);
-	}
-	console.log(service + " server started successfully");
-	ready++;
+    if(error){
+        console.log("Starting " + service + " server failed for the following error:");
+        console.log(error);
+        return process.exit(1);
+    }
+    console.log(service + " server started successfully");
+    ready++;
 
-	if(ready == 3){
-		ready++;
-		console.log("All servers started, downgrading from root to nobody")
-		process.setgid("nogroup");
-		process.setuid("nobody");
-	}
+    if(ready == 3){
+        ready++;
+        console.log("All servers started, downgrading from root to nobody");
+        process.setgid("nogroup");
+        process.setuid("nobody");
+    }
 }
